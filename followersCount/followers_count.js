2ff320c94e7d5ea937b55c7bd139d276df72f26b
Basil <basilhdib@gmail.com>
2024-05-06 10:25:28 -0400

    message

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -17,4 +17,10 @@ function checkCountValue() {
 function displayCount() {
         document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
         }
+    
+function resetCount(){
+    count = 0;
+   displayCount();
+   alert("Your follower count has been reset!")
+}
 
